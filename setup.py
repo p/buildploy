18e895aaefc37b1d6a81289f7886ec3cf2bd42c8
Oleg Pudeyev <oleg@bsdpower.com>
2016-02-16 00:09:10 -0500

    Add a binary wrapper and a setup.py to install it

---
+++ setup.py
@@ -0,0 +1,31 @@
+import os.path
+from distutils.core import setup
+
+package_name = 'buildploy'
+package_version = '0.1.0'
+
+data_files = ['LICENSE', 'README.md']
+
+setup(name=package_name,
+    version=package_version,
+    description='Build projects into deployment repositories',
+    author='Oleg Pudeyev',
+    author_email='oleg@bsdpower.com',
+    url='http://github.com/p/buildploy',
+    py_modules=['buildploy'],
+    scripts=['bin/buildploy'],
+    data_files=[
+        (data_files),
+    ],
+    license="BSD",
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+    ],
+)
